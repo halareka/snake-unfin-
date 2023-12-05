3b32dd60a5d73b76c178e7c236198368f0295c44
halareka <149045655+halareka@users.noreply.github.com>
2023-12-05 17:32:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,15 +33,37 @@ document.addEventListener('keypress', function (event) {
 function right(len){
 
     while(true){
+        if(i == 10){document.getElementById(A + 1).style.background = "lime";}
+
         i = i+1;
         console.log(i);
         document.getElementById(A + i).style.background = "lime";
         console.log(A + 1);
         if(i >= len){
             let il = i - 3;
-            document.getElementById(A + il).style.background = "red";
+            document.getElementById(A + il).style.background = "white";
+            il = 0;
+
 
         }
+        if(i > 9){
+            i = 0;
+            let il = i + 4;
+            document.getElementById(A + il).style.background = "white";
+            il = il + 1;
+            document.getElementById(A + il).style.background = "white";
+            il = il + 1;
+            document.getElementById(A + il).style.background = "white";
+            il = il + 1;
+            document.getElementById(A + il).style.background = "white";
+            il = il + 1;
+            document.getElementById(A + il).style.background = "white";
+            il = il + 1;
+            document.getElementById(A + il).style.background = "white";
+            
+
+        }
+
         break;
         
 
